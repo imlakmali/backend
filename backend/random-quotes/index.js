574dc8ea07acfa819da38b291ccc4af7ba6f4ab3
Lakmali Koralage <koralagelak573@gmail.com>
2024-06-27 11:47:47 +0530

    done

--- backend/random-quotes/index.js
+++ backend/random-quotes/index.js
@@ -4,43 +4,55 @@ const app = express();
 const port = 8080;
 let fileData;
 
+
 app.use(express.json());
 app.use(express.static("public"));
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
 fs.readFile("quotes.json", (err, data) => {
   fileData = JSON.parse(data);
 });
 
 app.post("/quotes", (req, res) => {
+
   if (req.body.type && req.body.type == "random") {
     let randomInx = generateRandomNumber(0, fileData.length);
     let selectedQuote = fileData[randomInx];
 
     res.send(selectedQuote);
+
   } else if (req.body.type && req.body.type == "search") {
+
     let keyword = req.body.keyword.toLowerCase();
     let filtedQuotes = fileData.filter((fileData) =>
       fileData.quote.toLowerCase().includes(keyword)
     );
+
     if (filtedQuotes.length > 0) {
+
       let randomIndex = Math.floor(Math.random() * filtedQuotes.length);
       let selectedQuotes = filtedQuotes[randomIndex];
       res.send(selectedQuotes);
+
     }else{
+
       res.send({message:"No quotes found"})
     }
     
   }else{
+
     res.send({message:"No quotes found"});
   }
 
 });
 
 function generateRandomNumber(lowLimit, upperLimit) {
+  
   let randomNum =
     Math.floor(Math.random() * (upperLimit - lowLimit)) + lowLimit;
 
